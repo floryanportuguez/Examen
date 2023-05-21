e40901ea01d5b9e3e6cecec11c8e21c3f2cc91ee
Floryan <floryanportuguez@gmail.com>
2023-05-20 20:54:31 -0600

    Insercion de detalles

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,32 +1,46 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import React from 'react';
 import Head from 'next/head';
-import { aboutMeData } from '../components/data/data'
+import { aboutMeData } from '../components/data/data';
 import { FaLinkedin } from 'react-icons/fa';
-
 
 const AboutMePage = () => {
   return (
-
     <main className='flex flex-col items-center'>
       <section className='flex flex-row w-10/12 mt-10 mb-6 items-center'>
         <div className="w-1/3">
-          <Image width={180} height={200} aria-label={" image of 1"} alt={" 1"} src={'/img/1.jpeg'} priority={false}
+          <Image
+            width={180}
+            height={200}
+            aria-label={" image of 1"}
+            alt={" 1"}
+            src={'/img/1.jpeg'}
+            priority={false}
             className='profileImage'
           />
           <h2>
-            <li> <a href="https://www.linkedin.com/in/floryan-portuguez-617006277" target="_blank" rel="noopener noreferrer">LinkedIn: <FaLinkedin /></a></li>
+            <li>
+              <a href="https://www.linkedin.com/in/floryan-portuguez-617006277" target="_blank" rel="noopener noreferrer">
+                LinkedIn: <FaLinkedin />
+              </a>
+            </li>
           </h2>
         </div>
 
         <div className='ml-20 w-2/3'>
           <h2 className="">{aboutMeData.title}</h2>
-          <p className='text-lg '>{aboutMeData.body}</p>
+          <p className='text-lg'>{aboutMeData.body}</p>
+
+          <h2>Skills</h2>
+          <p> <li>{aboutMeData.highlightbody}</li></p>
+
+          <h2>Academic training:</h2>
+          <p> <li>Bachelor of Business Informatics</li></p>
+         <p> <li>Costa Rica university</li></p>
         </div>
-
       </section>
-
     </main>
-  )
-}
+  );
+};
+
 export default AboutMePage;
--- src/app/components/data/data.tsx
+++ src/app/components/data/data.tsx
@@ -27,14 +27,15 @@ export const pages = [
 
 export const home = {
     title: "Welcome to my portfolio, where you can learn more about me and explore my work.",
-    body: "In this portfolio, you will find a selection of my most outstanding projects. Each one of them represents a unique challenge and an opportunity to demonstrate my ability to innovate and deliver exceptional results. You'll also see my focus on effective communication and collaboration with clients, as I believe understanding their needs and visions is critical to creating something truly exceptional."
+    body: "In this portfolio, you will find a selection of my most outstanding projects. Each one of them represents a unique challenge and an opportunity to demonstrate my ability to innovate and deliver exceptional results. You'll also see my focus on effective communication and collaboration with clients, as I believe understanding their needs and visions is critical to creating something truly exceptional.",
+    
 }
 
 export const aboutMeData = {
     name: "Portfolio",
     title: " Hello I´m Floryan Portuguez Vargas",
     body: " I am a passionate and creative systems technician. My main focus is to get to the solution as soon as possible. I also love designing websites, and I'm always looking for opportunities to apply my unique vision and personal style to every project I work on. I firmly believe in the importance of combining functionality with aesthetics, thus creating impressive experiences and designs.In addition to my professional work, I also spend time expanding my knowledge and skills. I'm always exploring new techniques and trends in computing to keep up to date and offer cutting-edge solutions to my clients.",
-
+    highlightbody: "Network knowledge, Databases, Problem resolution ,online administration, User Support"
 };
 
 
